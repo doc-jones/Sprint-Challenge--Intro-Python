32cb23f387a0ccba5c4f9f4d6be80ad7d47944a7
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-11-01 14:19:44 -0400

    Final final all test passed!

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -22,12 +22,11 @@ class GroundVehicle():
 
 # TODO
 class Motorcycle(GroundVehicle):
-    def __inti__(self):
-        super().__init__(num_wheels=2)
+    def __init__(self):
+        super().__init__(2)
 
     def drive(self):
-        return('BRAAAP!!')
-
+        return("BRAAAP!!")
 
 vehicles = [
     GroundVehicle(),
